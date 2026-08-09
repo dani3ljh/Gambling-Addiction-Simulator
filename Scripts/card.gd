1f9eedb0a6aaae654ae9de1abfb9d71e9344d52f
Daniel Hart <dnljh15@gmail.com>
2026-08-09 16:03:00 -0400

    Added Variables lowk

--- Scripts/card.gd
+++ Scripts/card.gd
@@ -1,6 +1,14 @@
 extends Node3D
 
+@export_group("Flip Animation")
+@export var start_y: float
+@export var end_y: float
+@export var flip_time: float
+
 @onready var front: AnimatedSprite3D = $Front
+
+var is_flipping = false
+var timer: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
